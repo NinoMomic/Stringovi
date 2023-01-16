773f34776c999c1f16cc72a87ca134ca1e3bc5dc
NinoMomic <58861649+NinoMomic@users.noreply.github.com>
2023-01-17 00:06:17 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string brojevi;
+
+            Console.WriteLine("Unesite jedan cijeli i jedan decimalni broj odvojeni s razmakom: ");
+            brojevi = Console.ReadLine();
+
+            int x;
+            double y;
+
+            string[] broj = brojevi.Split(' ');
+            x = Convert.ToInt32(broj[0]);
+            y = Convert.ToDouble(broj[1]);
+
+            Console.WriteLine("Prvi broj je " + x + " i drugi je " + y);
+
+            Console.ReadKey();
+        }
+    }
+}
