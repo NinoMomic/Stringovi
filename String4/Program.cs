497b3f0ad4b76661c4fa7d2fed48c5fa265f2e20
NinoMomic <58861649+NinoMomic@users.noreply.github.com>
2023-01-17 18:31:58 +0100

    Add files via upload

---
+++ String4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite rečenicu: ");
+            string recenica = Console.ReadLine();
+            string[] rijeci = recenica.Split();
+            Console.WriteLine(rijeci.First() + " " + rijeci.Last());
+
+            Console.ReadKey();
+        }
+    }
+}
